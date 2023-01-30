072e5c23aa158fd36eeb463655d147fed765297f
Alexander Kastler <github@xaka.eu>
2023-01-30 10:18:22 +0100

    init

---
+++ src/NumberSheetGenerator/PdfGenerator.cs
@@ -0,0 +1,73 @@
+﻿using iText.IO.Font.Constants;
+using iText.Kernel.Font;
+using iText.Kernel.Geom;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using System;
+using System.IO;
+
+namespace NumberSheetGenerator
+{
+    internal class PdfGenerator
+    {
+        //  @todo config
+        private const int _regularFontSize = 20;
+
+        private static byte[] CreatePdf(Action<PdfDocument, Document> decorate)
+        {
+            byte[] bytes;
+            using (var memoryStream = new MemoryStream())
+            {
+                using (var pdfWriter = new PdfWriter(memoryStream))
+                using (var pdfDoc = new PdfDocument(pdfWriter))
+                using (var doc = new Document(pdfDoc))
+                {
+                    pdfDoc.SetDefaultPageSize(pageSize: PageSize.A4);
+                    doc.SetFont(PdfFontFactory.CreateFont(StandardFonts.HELVETICA));
+                    //  @todo Margins!
+                    //doc.SetMargins(settings.PageMargins.Top, settings.PageMargins.Right, settings.PageMargins.Bottom, settings.PageMargins.Left);
+
+                    decorate(pdfDoc, doc);
+                }
+
+                bytes = memoryStream.GetBuffer();
+            }
+            return bytes;
+        }
+
+        public PdfGenerator()
+        {
+        }
+
+        public byte[] GenerateNumberSheet(int maxNumber)
+        {
+            return CreatePdf((pdfDoc, doc) =>
+            {
+                var numbersTable = new Table(5)
+                    .UseAllAvailableWidth()
+                    .SetKeepTogether(true)
+                    .
+                    //.SetExtendBottomRowOnSplit(true)
+                    .SetFontSize(_regularFontSize);
+
+                for (var number = 0; number < maxNumber; number++)
+                {
+                    var p = new Paragraph($"{number}")
+                        .SetBold()
+                        .SetKeepTogether(true);
+
+                    var cell = new Cell()
+                        .SetTextAlignment(iText.Layout.Properties.TextAlignment.CENTER)
+                        .SetVerticalAlignment(iText.Layout.Properties.VerticalAlignment.MIDDLE)
+                        .SetHeight(60)
+                        .Add(p);
+
+                    numbersTable.AddCell(cell);
+                }
+
+                doc.Add(numbersTable);
+            });
+        }
+    }
+}
---
+++ src/NumberSheetGenerator/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace NumberSheetGenerator
+{
+    public static class Program
+    {
+        private const string _pdfReaderApplicationPath = @"c:\Program Files (x86)\Adobe\Acrobat Reader DC\Reader\AcroRd32.exe";
+
+        public static void Main()
+        {
+            var generator = new PdfGenerator();
+            var pdfBytes = generator.GenerateNumberSheet(750);
+
+            var fileName = "numbers.pdf";
+            File.WriteAllBytes(fileName, pdfBytes);
+
+            var filePath = Path.Combine(Environment.CurrentDirectory, fileName);
+            
+            Process p = new Process
+            {
+                StartInfo = new ProcessStartInfo()
+                {
+                    FileName = _pdfReaderApplicationPath,
+                    Arguments = filePath
+                }
+            };
+            p.Start();
+        }
+    }
+}
